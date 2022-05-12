f44e3b57578419d312fb4beaa69c931373645b75
Lautaro Desouches <lautarodesouches@outlook.com>
2022-05-12 18:17:00 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
+import { home } from "routes/Routes";
 import { bgPrimary } from "theme/Colors";
 
 const Footer = () => {
-    return(
-        <footer className={`${bgPrimary}`}>
 
+    const date = new Date;
+
+    return(
+        <footer className={`${bgPrimary} text-white flex justify-around py-1`}>
+            <div>
+                <Link to={home}>Contacto</Link>
+            </div>
+            <div>
+                <Link to={home}>Privacidad</Link>
+            </div>
+            <div>
+                {date.getFullYear()}
+            </div>
         </footer>
     );
 }
